8f3f7ce2d0089617167505c393c43997487c09d4
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2023-08-06 22:32:43 +0530

    Create bmi.py

---
+++ bmi/bmi.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+from settings import *
+class App(ctk.CTK):
+  def __iniit__():
+
+    #window setup
+    super().__init__(fg_color= GREEN)
+    self.tilte('')
+    self.iconbitmap('empty.ico')
+    self.geometr('400x400')
+
+    
+    self.mainloop()
+
+App()
