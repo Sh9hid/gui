cfb1419415f64fb4e8fded038443da3ecf2516b2
Abdullah Shahid Khan <69131903+Sh9hid@users.noreply.github.com>
2023-08-06 22:57:15 +0530

    Update bmi.py

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -1,15 +1,31 @@
 import customtkinter as ctk
 from settings import *
-class App(ctk.CTK):
-  def __init__():
+try:
+  from ctypes import windll, byref, sizeof, c_int
+except:
+  pass
+class App(ctk.CTk):
+  def __init__(self):
 
     #window setup
     super().__init__(fg_color= GREEN)
-    self.tilte('')
-    self.iconbitmap('empty.ico')
-    self.geometr('400x400')
-
-    
+    self.title('')
+    self.geometry('400x400')
+    #self.iconbitmap('empty.ico')
+    self.resizable(False, False)
+    self.change_title_bar_color()
     self.mainloop()
 
+  def change_title_bar_color(self):
+    try:
+      HWND = windll.user32.GetParent(self.winfo_id())
+      DWMWA_ATTRIBUTE = 35
+      COLOR = TITLE_HEX_COLOR
+      windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
+    except:
+      pass
+    
+    
+    
+    
 App()
